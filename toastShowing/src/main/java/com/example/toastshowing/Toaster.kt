5d7e8dd873ce9294c22e176344eaf32e5ef3533a
Himanshu Verma <vhimanshu0798@gmail.com>
2025-04-30 22:12:28 +0530

    added new toast message in toaster

--- toastShowing/src/main/java/com/example/toastshowing/Toaster.kt
+++ toastShowing/src/main/java/com/example/toastshowing/Toaster.kt
@@ -7,6 +7,7 @@ class Toaster {
     companion object {
         fun showToast(context: Context, msg: String) {
             Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
+            Toast.makeText(context,"This is second version",Toast.LENGTH_SHORT).show()
         }
     }
 }
